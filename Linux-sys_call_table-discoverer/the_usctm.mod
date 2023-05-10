15c293568db4d3ce4509c6cf2ea044e9737e1672
luca-capotombolo <capoluca98@gmail.com>
2023-05-10 18:30:14 +0200

    Aggiunto modulo

---
+++ Linux-sys_call_table-discoverer/the_usctm.mod
@@ -0,0 +1,2 @@
+/home/cap/Scaricati/codici-soa/KERNEL-PROGRAMMING-BASICS/SYS-CALL-TABLE-DISCOVERY/Linux-sys_call_table-discoverer/usctm.o /home/cap/Scaricati/codici-soa/KERNEL-PROGRAMMING-BASICS/SYS-CALL-TABLE-DISCOVERY/Linux-sys_call_table-discoverer/./lib/vtpmo.o
+
---
+++ Linux-sys_call_table-discoverer/the_usctm.mod.c
@@ -0,0 +1,48 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+BUILD_LTO_INFO;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0xcb440b5e, "module_layout" },
+	{ 0x55dc419c, "param_ops_int" },
+	{ 0xf95e0fb3, "pv_ops" },
+	{ 0xe2d5255a, "strcmp" },
+	{ 0x52673010, "current_task" },
+	{ 0x109274ed, "iterate_mounts" },
+	{ 0x7cd8d75e, "page_offset_base" },
+	{ 0x87a21cb3, "__ubsan_handle_out_of_bounds" },
+	{ 0xd0da656b, "__stack_chk_fail" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xbdfb6dbb, "__fentry__" },
+	{ 0xd27052e9, "param_array_ops" },
+	{ 0x7feccf4f, "param_ops_ulong" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "6C5A60502C0D8D7E8E6BFD7");
